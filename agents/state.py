494245fb16596cd604a13b0d845debc5d9a968e4
jobanxd <sethjovan.pacturanan@gmail.com>
2026-02-20 22:18:21 +0800

    state management for agents

---
+++ agents/state.py
@@ -0,0 +1,8 @@
+"""State management for Agents."""
+from typing import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+
+class AgentState(TypedDict):
+    """State of the Agents"""
+    messages: Annotated[list, add_messages]
+    next_agent: str
